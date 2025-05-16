991ffcb2e68ece7afccb5727dc6184e5f7555f8f
Ruvimbo Tanyaradzwa Makayi <ruvimbomakayi0@gmail.com>
2025-05-16 20:42:07 +0200

    queries/

---
+++ query_statements.sql
@@ -0,0 +1,6 @@
+USE smartfarmingdb;
+-- Retrieve the names and locations of all farms.
+SELECT FarmName, Location FROM farms;
+
+-- List all crops and their planting seasons.
+SELECT CropName, PlantingSeason FROM crops;
